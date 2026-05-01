1eb454ee7928131d08d672383140fb57fd72ae25
Nobody <ahouseman1000@gmail.com>
2026-05-01 16:38:13 -0500

    Added my database schema

--- app/models.py
+++ app/models.py
@@ -1,9 +1,38 @@
 from .extensions import db
 
 
-class ExampleRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
+class Techs(db.Model):
+    techEmployeeID = db.Column(db.String(10), primary_key=True)
+    techName = db.Column(db.String(32), nullable=False)
 
     def __repr__(self):
-        return f"<ExampleRecord {self.title}>"
+        return f"<Tech {self.title}>"
+
+class Users(db.Model):
+    userEmployeeID = db.Column(db.String(10), primary_key=True)
+    userName = db.Column(db.String(32), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.title}>"
+
+class Inventory(db.Model):
+    itemID = db.Column(db.String(15), primary_key=True)
+    itemDescription = db.Column(db.String(20), nullable=False)
+    assignedUser = db.Column(db.String(10), db.ForeignKey("users.userEmployeeID"), nullable=False)
+    rotationDate = db.Column(db.Date)
+
+
+    def __repr__(self):
+        return f"<Inventory {self.title}>"
+
+class Tickets(db.Model):
+    ticketID = db.Column(db.String(20), primary_key=True)
+    submittingUser = db.Column(db.String(10), db.ForeignKey("users.userEmployeeID"), nullable=False)
+    ticketText = db.Column(db.String(100))
+    ticketItem = db.Column(db.String(15), db.ForeignKey("inventory.itemID"), nullable=False)
+    assignedTech = db.Column(db.String(10), db.ForeignKey("techs.techEmployeeID"), nullable=False)
+    ticketDate = db.Column(db.Date, nullable=False)
+
+
+    def __repr__(self):
+        return f"<User {self.title}>"
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
 
-from .models import ExampleRecord
+from .models import Techs
+from .models import Users
+from .models import Inventory
+from .models import Tickets
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    records = ExampleRecord.query.order_by(ExampleRecord.id.desc()).all()
+    records = Techs.query.order_by(Techs.techEmployeeID.desc()).all()
     return render_template("index.html", records=records)
